2ba7497d3d15bdada1ced65ee5be3ca5daced99a
kenpeter <figo2476@hotmail.com>
2017-03-26 00:09:50 +1100

    update

---
+++ README.md
@@ -0,0 +1,14 @@
+## Intro
+
+Srape seek.com.au data and display data on android phone with react native.
+
+## Misc
+* https://facebook.github.io/react-native/docs/using-a-listview.html
+* https://www.tutorialspoint.com/react_native/react_native_flexbox.htm
+* https://hackernoon.com/react-native-new-flatlist-component-30db558c7a5b#.2fwqovkyk
+* https://github.com/dabit3/react-native-hackathon-starter
+
+* https://daveceddia.com/ajax-requests-in-react/
+* https://stackoverflow.com/questions/29642685/maintain-aspect-ratio-of-image-with-full-width-in-react-native
+* https://docs.expo.io/versions/v15.0.0/guides/building-standalone-apps.html
+* https://expo.io/@kenpeter/osmosis_seek_android
---
+++ components/ListComponent.js
@@ -0,0 +1,115 @@
+// need react and component
+import React, { Component } from 'react';
+// text from native
+import {
+  Text,
+  ScrollView,
+  View,
+  StyleSheet,
+
+  TouchableHighlight,
+  Dimensions,
+  Linking
+} from 'react-native';
+
+//
+import Image from 'react-native-scalable-image';
+
+// card, list and button from react native element
+import { Card, ListItem, Button, Tile } from 'react-native-elements'
+
+import colors from 'HSColors';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import axios from 'axios';
+
+import OpenURLButton from './OpenURLButton';
+
+// empty style
+let styles = {};
+
+// export my component
+class ListComponent extends Component {
+  // constructor with props
+  constructor(props) {
+    // parent props
+    super(props);
+
+    this.state = {
+      lists: []
+    };
+  }
+
+  componentDidMount() {
+    let url = 'http://hljback.shopshop.space/product/list';
+    axios
+      .get(url)
+      .then(res => {
+        // It uses data
+        this.setState({ lists: res.data });
+      });
+  }
+
+  render() {
+    //const myCard
+
+    return (
+      <ScrollView style={{backgroundColor: 'white'}}>
+
+        <View style={styles.headingContainer}>
+          <Image
+            width={Dimensions.get('window').width}
+            source={ {uri: 'https://s-media-cache-ak0.pinimg.com/originals/73/cf/14/73cf142f26657abe09d9d42761f39ab2.jpg'} }
+          />
+        </View>
+
+        <View style={styles.container}>
+          {
+            this.state.lists.map((u, i) => {
+              const myText = u.title + '\n' +
+                u.price;
+              const theUrl = 'http://hljback.shopshop.space/imgs/' + u.productId + '/' + u.imgs[0];
+
+              return (
+                <Card
+                  key={i}
+                  title={u.title}
+                  image={theUrl}>
+                >
+
+                  <Text style={{marginBottom: 10}}>
+                    hi
+                  </Text>
+                  <Button
+                      icon={{name: 'code'}}
+                      backgroundColor='#03A9F4'
+                      buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                      title='VIEW NOW' />
+                </Card>
+              );
+            })
+          }
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+styles = StyleSheet.create({
+  container: {
+    margin: 15
+  },
+  headingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.grey2
+  },
+  heading: {
+    color: 'white',
+    marginTop: 10,
+    fontSize: 22
+  }
+})
+
+export default ListComponent;
